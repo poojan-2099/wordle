201565708a1840c6c642f3d55bee5d53bfe3a7ff
Patel <poojan-bharatbhai.patel.047@my.csun.edu>
2022-05-19 14:32:57 -0700

    keyboard added

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Key from './Key';
 
 function Keyboard() {
 
@@ -8,9 +9,24 @@ function Keyboard() {
 
   return (
     <div className='keyboard'>
-      <div className='line1'></div>
-      <div className='line2'></div>
-      <div className='line3'></div>
+      <div className='line1'>
+          {keys1.map((key) => {
+            return <div> <Key keyVal={key} /> </div>;
+          })}
+      </div>
+      <div className='line2'>
+          {keys2.map((key) => {
+            return <div> <Key keyVal={key} /> </div>;
+          })}
+      </div>
+      <div className='line3'>
+
+          <Key keyVal={"ENTER"} bigKey />
+          {keys3.map((key) => {
+            return <div> <Key keyVal={key} /> </div>;
+          })}
+          <Key keyVal={"DELETE"} bigKey />
+      </div>
     </div>
   )
 }
